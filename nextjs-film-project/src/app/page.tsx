350309fe2dd36a9e648f7a978e2b12b7b287826d
Esra Kelleci <83169230+esrakllci@users.noreply.github.com>
2024-10-29 21:39:05 +0300

    FEAT:create-homepage

--- nextjs-film-project/src/app/page.tsx
+++ nextjs-film-project/src/app/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
-  return <div>Home</div>;
+//import React from "react";
+
+//import HomeContainer from "/containers/home";
+
+function HomePage() {
+  //return <HomeContainer/>;
 }
+
+export default HomePage;
